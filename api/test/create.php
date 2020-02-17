acf2d20304d09641bf047fc79b827915204ffa8b
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-02-17 12:21:42 +0100

    cont work with making post to work from app

--- api/test/create.php
+++ api/test/create.php
@@ -2,7 +2,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, AUthorization, X-Requested-With');
+header('Access-Control-Allow-Headers: *');
+// header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
  
 // //FELHANTERING FRÅN ANDERS
